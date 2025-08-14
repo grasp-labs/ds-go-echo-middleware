b3e655310cbdab97fd649ce385988bedd46dacf8
skerve <arve.skjervheim@gmail.com>
2025-08-14 21:58:51 +0200

    export claims

---
+++ middleware/claims/claims.go
@@ -0,0 +1,6 @@
+package claims
+
+import internal "github.com/grasp-labs/ds-go-echo-middleware/middleware/internal/models"
+
+// Re-export the internal type so other modules can use it safely.
+type Context = internal.Context
--- middleware/requestctx/user.go
+++ middleware/requestctx/user.go
@@ -3,18 +3,18 @@ package requestctx
 import (
 	"context"
 
-	"github.com/grasp-labs/ds-go-echo-middleware/middleware/internal/models"
+	"github.com/grasp-labs/ds-go-echo-middleware/middleware/claims"
 )
 
 const userContextKey ctxKey = "userContext"
 
 // GetUserContext returns the Context model from context.
-func GetUserContext(ctx context.Context) *models.Context {
-	val := ctx.Value(userContextKey).(*models.Context)
+func GetUserContext(ctx context.Context) *claims.Context {
+	val := ctx.Value(userContextKey).(*claims.Context)
 	return val
 }
 
 // SetRequestID sets the request ID in the context.
-func SetUserContext(ctx context.Context, userContext *models.Context) context.Context {
+func SetUserContext(ctx context.Context, userContext *claims.Context) context.Context {
 	return context.WithValue(ctx, userContextKey, userContext)
 }
