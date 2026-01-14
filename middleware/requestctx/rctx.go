3e03c25c62e156ee60ac29e3f95ed52269c2bc13
Jan Odvar Grindheim <jan.odvar@protonmail.com>
2026-01-14 15:51:54 +0100

    refactor: move requestctx and logctx into middleware.

--- middleware/requestctx/rctx.go
+++ middleware/requestctx/rctx.go
@@ -5,13 +5,10 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/grasp-labs/ds-go-echo-middleware/v2/middleware/claims"
-
-	"github.com/labstack/echo/v4"
 )
 
 type RequestCtx struct {
 	Ctx       context.Context
-	C         echo.Context
 	RequestID uuid.UUID
 	Locale    string
 	Claims    *claims.Context
@@ -19,33 +16,35 @@ type RequestCtx struct {
 	Err       error
 }
 
-func (r *RequestCtx) New(c echo.Context, locale string) RequestCtx {
-	ctx := c.Request().Context()
-	claims := GetUserContext(ctx)
+func New(c context.Context, locale string) RequestCtx {
+	claims := GetUserContext(c)
 
-	var tenantID uuid.UUID
-	var err error
+	var (
+		tenantID uuid.UUID = uuid.Nil
+		err      error
+	)
+
 	if claims != nil {
 		tenantID, err = claims.GetTenantId()
+
 		if err != nil {
 			tenantID = uuid.Nil
+		} else {
+			tenantID = uuid.Nil
 		}
-	} else {
-		tenantID = uuid.Nil
 	}
 
-	requestID, err := uuid.Parse(GetRequestID(ctx))
+	requestID, err := uuid.Parse(GetRequestID(c))
 	if err != nil {
 		requestID = uuid.New()
 	}
 
 	return RequestCtx{
-		Ctx:       ctx,
-		C:         c,
-		RequestID: requestID,
+		Ctx:       c,
 		Locale:    locale,
 		Claims:    claims,
 		TenantID:  tenantID,
+		RequestID: requestID,
 		Err:       err,
 	}
 }
